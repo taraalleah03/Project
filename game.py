63e35348623ae4d33972efaf03f20cb2849f0d6b
tarahere <mtaraalleah@gmail.com>
2025-10-01 17:45:10 +0300

    Project base, to be adjusted if needed

---
+++ game.py
@@ -0,0 +1,24 @@
+#import all necessary modules/libraries
+
+#Introduction to the game
+
+
+#User inputs name
+#Rules of the game
+
+
+#Main game
+
+#List of countries to choose from (get the list of countries from database)
+
+
+#To move to another country solve a puzzle
+
+
+#If the cow loses all three lives or if the timer runs out =  the farmer catches the cow.
+
+
+#Ending
+
+#If the cow wins the game prints out “ Farmer in jail “
+#If the cow loses the farmer says something like “Dinner time”
