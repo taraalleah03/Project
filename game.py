51696b72edbef7f91901386d88bd69693b4427b1
awahn <awahnde.princess@gmail.com>
2025-10-03 14:02:41 +0300

    continue!

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 #import all necessary modules/libraries
 import mariadb
-import timer
+import time
+import random
 connection = mariadb.connect(
     host = '127.0.0.1',
     port = 3306,
@@ -8,7 +9,19 @@ connection = mariadb.connect(
     password = 'princess',
     database = 'flight_game',
     autocommit = True)
-print("Connected to Maria db!")
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = f"{mins:02d}:{secs:02d}"
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("Time's up, the farmer got you!")
+    countdown_timer(90)
+
+#Connect to database
+
 
 #Introduction to the game
 
