f2fa3f431139ae2cf62b18bdfa398a78066c27b5
tranpham <clary.phamvb@gmail.com>
2025-10-03 14:40:47 +0300

    hi1

--- game.py
+++ game.py
@@ -44,9 +44,11 @@ print("The cow needs to solve puzzles to travel to another country!")
 print("You have 3 lives and 90 seconds. Wrong answer = lose 1 life.")
 
 while lives > 0:
-
+### Timer
     elapsed = time.time() - start_time
     remaining_time = int(time_limit - elapsed)
+
+### if the timer runs out = farmer catches the cow = lose
     if remaining_time <= 0:
         print("\nTime’s up! The farmer catches the cow. Dinner time!")
         break
@@ -55,7 +57,7 @@ while lives > 0:
     if question_count % 3 == 0:
         print(f"\n Announcement: {remaining_time} seconds left! Hurry up!")
 
-
+### Puzzle solving
     puzzle, answer = random.choice(list(puzzles.items()))
     user_answer = input(f"\n Puzzle: {puzzle} ").lower().strip()
 
@@ -63,17 +65,24 @@ while lives > 0:
         print("Correct! You can travel to another country.")
         country: input("Which country do you want to go to? ").strip()
         print(f"The cow travels to {country}")
-    else:
+        if country == best_country: #if the user guess the right country
+            print(f"The cow found the best grass in {best_country}!")
+            print("Farmer in jail! You win!")
+            break
+    else: #if thr answer is wrong, lose 1 live and the system gives a hint
         lives -= 1
         print(f"Wrong! You lose a life.Lives left: {lives}")
+        hint = random.choice(hints[best_country])
+        print(f" Here is a hint your you: {hint}")
         if lives > 0:
             next_country = input("Choose another country to try: ").strip()
             print(f"The cow travels to {next_country}")
 
+#If the cow loses all three lives =  the farmer catches the cow.
 if lives == 0:
-    print("\n You have no lives left. The farmer catched the cow!!! Dinner time!")
+    print("\n You have no lives left. The farmer catches the cow!!! Dinner time!")
 
-#If the cow loses all three lives or if the timer runs out =  the farmer catches the cow.
+
 
 
 #Ending
