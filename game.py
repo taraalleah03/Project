900254db68d9875f0ee12f785462e3ace25d0807
tarahere <mtaraalleah@gmail.com>
2025-10-05 21:43:11 +0300

    update

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ connection = mariadb.connect(
     host = '127.0.0.1',
     port = 3306,
     user = 'root',
-    password = 'taRamart!n2003thirty',
+    password = 'princess',
     database = 'flight_game',
     autocommit = True)
 #visual intro
